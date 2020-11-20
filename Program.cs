980a67fdeae028330865baefcdedc1fbcffaeee2
Travis Pinson <travispinson@gmail.com>
2020-11-20 10:39:30 -0600

    Phase 1 Complete. Gather initial props

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace plan_your_heist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teammate alpha = new Teammate();
+
+            Console.WriteLine("Plan Your Heist!");
+            Console.WriteLine("Enter a name for a new teammate:");
+            string proposedName = Console.ReadLine();
+            alpha.AddName(proposedName);
+
+
+            Console.WriteLine("Enter teammate Skill Level: ");
+            string proposedSkillInput = Console.ReadLine();
+            int proposedSkill = int.Parse(proposedSkillInput);
+            alpha.AddSkillLevel(proposedSkill);
+
+            Console.WriteLine("Enter teammate courage: ");
+            string proposedCourageInput = Console.ReadLine();
+            double proposedCourage = Convert.ToDouble(proposedCourageInput);
+            alpha.AddCourage(proposedCourage);
+
+            Console.WriteLine(alpha.Name);
+            Console.WriteLine(alpha.SkillLevel);
+            Console.WriteLine(alpha.Courage);
+
+
+
+        }
+    }
+}
---
+++ Teammate.cs
@@ -0,0 +1,23 @@
+namespace plan_your_heist
+{
+    class Teammate
+    {
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public double Courage { get; set; }
+
+        public void AddName(string name)
+        {
+            Name = name;
+        }
+        public void AddSkillLevel(int skill)
+        {
+            SkillLevel = skill;
+        }
+
+        public void AddCourage(double rating)
+        {
+            Courage = rating;
+        }
+    }
+}
