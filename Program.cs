c8637e0129711adca00a3cf42444758ce81fb129
Travis Pinson <travispinson@gmail.com>
2020-11-20 14:10:50 -0600

    Phase 6 - Set difficulty and display report of wins / losses

--- Program.cs
+++ Program.cs
@@ -16,6 +16,9 @@ namespace plan_your_heist
                 Teammate alpha = new Teammate();
 
                 Console.WriteLine("Plan Your Heist!");
+
+
+
                 Console.WriteLine("Enter a name for a new teammate:");
                 string proposedName = Console.ReadLine();
                 if (proposedName == "")
@@ -27,10 +30,25 @@ namespace plan_your_heist
                     alpha.AddName(proposedName);
                 }
 
+
                 Console.WriteLine("Enter teammate Skill Level: ");
                 string proposedSkillInput = Console.ReadLine();
+
+                try
+                {
+                    Int16.Parse(proposedSkillInput);
+                }
+                catch
+                {
+                    Console.WriteLine("You didn't enter a number");
+                    break;
+                }
+
+
                 int proposedSkill = int.Parse(proposedSkillInput);
                 alpha.AddSkillLevel(proposedSkill);
+
+
 
                 Console.WriteLine("Enter teammate courage: ");
                 string proposedCourageInput = Console.ReadLine();
@@ -42,18 +60,36 @@ namespace plan_your_heist
 
             Console.WriteLine($"Team Members: {team.Count}");
 
-            int infinityZero = 0;
+            int foreverZero = 0;
 
-            while (infinityZero == 0)
+            while (foreverZero == 0)
             {
                 Console.WriteLine("How many trial runs should we go through?");
                 string timesToTryResponse = Console.ReadLine();
+
+
+                try
+                {
+                    Int16.Parse(timesToTryResponse);
+                }
+                catch
+                {
+                    Console.WriteLine("You didnt enter a number.");
+                    break;
+                }
+
                 int timesToTry = Int16.Parse(timesToTryResponse);
                 int counter = 0;
+
+                Console.WriteLine("How hard is the bank to rob on a scale of 1 to 100?");
+                string bankDifficultyResponse = Console.ReadLine();
+                int bankDifficulty = Int32.Parse(bankDifficultyResponse);
+                int wins = 0;
+                int losses = 0;
                 while (counter < timesToTry)
                 {
                     counter++;
-                    int bankDifficulty = 100;
+
 
                     List<int> teamSkillCatcher = new List<int>();
                     foreach (Teammate member in team)
@@ -69,19 +105,19 @@ namespace plan_your_heist
 
                     if (teamSkill > bankDifficulty)
                     {
-
                         Console.WriteLine("Successfully robbed bank");
+                        bankDifficulty = bankDifficulty - luckValue;
+                        wins++;
                     }
                     else
                     {
-
                         Console.WriteLine("You have failed. You bring shame to your family...");
+                        bankDifficulty = bankDifficulty - luckValue;
+                        losses++;
                     }
-
                 }
 
-
-
+                Console.WriteLine($"Successful Runs = {wins} --- Failed Runes = {losses}");
 
             }
 
